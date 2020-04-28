fa9d792cbb2feecbceb92f6c69856482d0a993d3
今福沙紀 <imafukusaki@mba.local>
2020-04-28 21:42:49 +0900

    トップページへのリンク設定

--- spec/system/products_spec.rb
+++ spec/system/products_spec.rb
@@ -7,14 +7,39 @@ describe '商品', type: :system do
       visit potepan_product_path(product.id)
     end
 
-    context 'EXAMPLE TOTEページ' do
-      it 'ページタイトルが正しく表示されている' do
+    context 'EXAMPLE TOTE詳細ページ' do
+      it 'ページタイトルが正しく表示される' do
         expect(page).to have_title   'EXAMPLE TOTE - BIGBAG Store'
       end
 
       it '商品の情報が表示される' do
         expect(page).to have_content 'EXAMPLE TOTE'
         expect(page).to have_content '$19.99'
+      end
+    end
+
+    context 'トップページに移動する' do
+      before do
+        visit potepan_product_path(product.id)
+      end
+
+      it 'ロゴをクリックする' do
+        click_on 'ロゴ画像'
+        expect(page).to have_title 'BIGBAG Store'
+      end
+
+      it 'ヘッダーのHOMEをクリックする' do
+        within '.navbar' do
+          click_on 'HOME'
+        end
+        expect(page).to have_title 'BIGBAG Store'
+      end
+
+      it '商品詳細ページ内のHOMEをクリックする' do
+        within '.pageHeader' do
+          click_on 'HOME'
+        end
+        expect(page).to have_title 'BIGBAG Store'
       end
     end
 
