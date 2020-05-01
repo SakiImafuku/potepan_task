bbdbae65c31b0bc4f42baa539f4983dbddda90e4
今福沙紀 <imafukusaki@mba.local>
2020-05-01 11:49:24 +0900

    商品一覧表示

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -1,5 +1,7 @@
 class Potepan::CategoriesController < ApplicationController
   def show
     @taxonomies = Spree::Taxonomy.all
+    @taxon = Spree::Taxon.find(params[:id])
+    @taxons = [@taxon] + @taxon.leaves
   end
 end
