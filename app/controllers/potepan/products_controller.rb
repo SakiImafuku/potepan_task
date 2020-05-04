239701f8ef421a9c263f722152c83879c1777ba5
今福沙紀 <imafukusaki@mba.local>
2020-05-04 14:31:58 +0900

    関連商品の表示

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -1,5 +1,8 @@
 class Potepan::ProductsController < ApplicationController
   def show
     @product = Spree::Product.find(params[:id])
+    @taxons = @product.taxons
+    @related_products = [@product]
+    @related_display_count = 4
   end
 end
