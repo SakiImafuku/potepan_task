f8cb2cd670657b95efded7bb47715e267f5266b7
今福沙紀 <imafukusaki@mba.local>
2020-05-09 20:46:14 +0900

    関連商品取得のメソッドを定義

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -1,5 +1,7 @@
 class Potepan::ProductsController < ApplicationController
   def show
     @product = Spree::Product.find(params[:id])
+    @taxons = @product.taxons
+    @related_products = @product.all_related_products.limit(4).includes(master: [:images, :default_price])
   end
 end
--- app/helpers/products_helper.rb
+++
@@ -1,7 +0,0 @@
-module ProductsHelper
-  def all_related_products(product)
-    product.taxons.map do |taxon|
-      taxon.all_products.includes(master: [:images, :default_price])
-    end
-  end
-end
