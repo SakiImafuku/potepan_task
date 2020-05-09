4303067b6057f451fd6ea1a20ea0848b2240d1ff
今福沙紀 <imafukusaki@mba.local>
2020-05-09 21:50:18 +0900

    rubocopによる修正

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -2,6 +2,7 @@ class Potepan::ProductsController < ApplicationController
   def show
     @product = Spree::Product.find(params[:id])
     @taxons = @product.taxons
-    @related_products = @product.all_related_products.limit(4).includes(master: [:images, :default_price])
+    @related_products = @product.all_related_products.limit(4).
+      includes(master: [:images, :default_price])
   end
 end
