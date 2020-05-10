93772695426d0949e2b208f889377d31ed66de17
今福沙紀 <imafukusaki@mba.local>
2020-05-10 13:45:07 +0900

    display_count変数の設定

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -1,7 +1,8 @@
 class Potepan::ProductsController < ApplicationController
   def show
     @product = Spree::Product.find(params[:id])
-    @related_products = @product.all_related_products.limit(4).
+    display_count = 4
+    @related_products = @product.all_related_products.limit(display_count).
       includes(master: [:images, :default_price])
   end
 end
