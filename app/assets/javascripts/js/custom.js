c839714964b34d04adcdc27541e4890112f74538
今福沙紀 <imafukusaki@mba.local>
2020-05-30 17:50:10 +0900

    体裁の修正

--- app/assets/javascripts/js/custom.js
+++ app/assets/javascripts/js/custom.js
@@ -33,21 +33,21 @@ jQuery(document).ready(function($) {
 });
 
 jQuery(document).ready(function($) {
-	    let navbar = $('.navbar-main'),
-	        distance = navbar.offset().top,
-	        $window = $(window);
-	    $window.scroll(function() {
-	        if(($window.scrollTop() >= distance) && ($(".navbar-default").hasClass("navbar-main")))
-	        {
-	            navbar.removeClass('navbar-fixed-top').addClass('navbar-fixed-top');
-	          	$("body").addClass("padding-top");
-	          	$(".topBar").css("display","none");
-	        } else {
-	            navbar.removeClass('navbar-fixed-top');
-	            $("body").removeClass("padding-top");
-	            $(".topBar").css("display","block");
-	        }
-	    });
+    let navbar = $('.navbar-main'),
+        distance = navbar.offset().top,
+        $window = $(window);
+    $window.scroll(function() {
+        if(($window.scrollTop() >= distance) && ($(".navbar-default").hasClass("navbar-main")))
+        {
+            navbar.removeClass('navbar-fixed-top').addClass('navbar-fixed-top');
+            $("body").addClass("padding-top");
+            $(".topBar").css("display","none");
+        } else {
+            navbar.removeClass('navbar-fixed-top');
+            $("body").removeClass("padding-top");
+            $(".topBar").css("display","block");
+        }
+    });
 });
 //============================== ALL DROPDOWN ON HOVER =========================
 jQuery(document).ready(function(){
