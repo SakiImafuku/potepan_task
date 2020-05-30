b7084cb9e542a9eef161c7a58081c5d01b5e8ad7
今福沙紀 <imafukusaki@mba.local>
2020-05-30 12:50:53 +0900

    定数の記載場所変更

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -1,5 +1,4 @@
 class Potepan::ProductsController < ApplicationController
-  MAX_DISPLAY_COUNT = 4
   def show
     @product = Spree::Product.find(params[:id])
     @related_products = @product.all_related_products.limit(MAX_DISPLAY_COUNT).
